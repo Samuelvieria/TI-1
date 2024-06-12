c726538b43525e266dba38055a4e1278fa159d1e
Samuel Vieira <147442998+Samuelvieria@users.noreply.github.com>
2024-06-12 12:31:19 -0300

    Add files via upload

---
+++ Piloto/script.js
@@ -0,0 +1,13 @@
+var imagem = document.querySelector('.pilotos');
+
+
+      function redirecionar(url) {
+  window.location.href = url;
+}
+function mostrarPopup() {
+			document.getElementById("popup").style.display = "block";
+		}
+
+
+
+
